7299a16c7662153d996cc8348fe8baa70d8172fd
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2016-07-29 06:23:05 -0500

    Add custom logger

--- go-rest-api.go
+++ go-rest-api.go
@@ -7,8 +7,11 @@ import (
 	"html"
 	"log"
 	"net/http"
+	"os"
 	"time"
 )
+
+var logger = log.New(os.Stdout, "", log.Ldate|log.Ltime|log.Lmicroseconds)
 
 type SampleResponse struct {
 	Id    string    `json:"id"`
@@ -17,19 +20,19 @@ type SampleResponse struct {
 }
 
 func TestTopLevelHandler(w http.ResponseWriter, r *http.Request) {
-	log.Printf("testTopLevelHandler %v", r)
+	logger.Printf("testTopLevelHandler %v", r)
 	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 }
 
 func TestIDHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	log.Printf("testIDHandler %v", r)
+	logger.Printf("testIDHandler %v", r)
 	fmt.Fprintf(w, "Hello, %q", html.EscapeString(vars["id"]))
 }
 
 func TestSubIDHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	log.Printf("testSubIDHandler %v", r)
+	logger.Printf("testSubIDHandler %v", r)
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	response := SampleResponse{
 		Id:    vars["id"],
@@ -37,17 +40,17 @@ func TestSubIDHandler(w http.ResponseWriter, r *http.Request) {
 		Time:  time.Now(),
 	}
 	if err := json.NewEncoder(w).Encode(response); err != nil {
-		log.Printf("err %v", err)
+		logger.Printf("err %v", err)
 		panic(err)
 	}
 }
 
 func main() {
-	log.Printf("starting")
+	logger.Printf("starting")
 	r := mux.NewRouter().StrictSlash(true)
 	s := r.PathPrefix("/test/v1").Subrouter()
 	s.HandleFunc("/", TestTopLevelHandler)
 	s.HandleFunc("/{id}", TestIDHandler)
 	s.HandleFunc("/{id}/sub/{subID}", TestSubIDHandler)
-	log.Fatal(http.ListenAndServe(":8080", r))
+	logger.Fatal(http.ListenAndServe(":8080", r))
 }
